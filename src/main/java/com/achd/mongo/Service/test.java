04f315255306297ec96dada1411a9ec8b07043ca
Yadeah <m13262298768@163.com>
2019-09-22 22:56:12 +0800

    首页图片

---
+++ src/main/java/com/achd/mongo/Service/test.java
@@ -0,0 +1,11 @@
+package com.achd.mongo.Service;
+
+public enum test {
+    有(1);
+
+    int desc;
+
+    test(int desc) {
+        this.desc = desc;
+    }
+}
--- src/vue-element-admin-master/src/views/ACHD/home/index.vue
+++ src/vue-element-admin-master/src/views/ACHD/home/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <span>冠心病队列研究平台</span>
+  <div class="block">
+    <el-carousel trigger="click" height="600px">
+      <el-carousel-item v-for="item in images" :key="item">
+        <img :src="item" style="height:100%; width: 100%">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      images: [
+        require('../../../assets/home1.jpg'),
+        require('../../../assets/home2.jpg'),
+        require('../../../assets/home3.png')
+      ]
+    }
+  }
+}
+</script>
