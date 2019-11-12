46372f0de7b262d83883e1429d1204ad751af689
RainkQ <rainkq@gmail.com>
2019-11-12 21:54:18 +0800

    添加生成测试数据方法

--- src/test/java/com/achd/mongo/MongoApplicationTests.java
+++ src/test/java/com/achd/mongo/MongoApplicationTests.java
@@ -1,31 +1,40 @@
 package com.achd.mongo;
 
+import com.achd.mongo.Entity.BDT.BDT_Sub.BDT;
+import com.achd.mongo.Entity.BDT.BDT_Sub.BDT_Record_Sub.Exercise;
 import com.achd.mongo.Entity.BaseEntity;
+import com.achd.mongo.Entity.CCTA.CCTA_Sub.CCTA;
 import com.achd.mongo.Service.DataService;
+import com.achd.mongo.Utilities.SomeUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class MongoApplicationTests {
+public class MongoApplicationTests {
     @Autowired
     DataService dataService;
 
     @Test
     public void contextLoads() {
-        if (true) {
+        if (false) {
             return;
         }
 
-        for (int num = 0; num < 1000; num++) {
-            BaseEntity baseEntity = new BaseEntity();
-            baseEntity.setId(String.valueOf(num));
-            baseEntity.set姓名(String.valueOf(num));
-            baseEntity.set性别(num % 2 == 0 ? "男" : "女");
 
+        SomeUtil someUtil = new SomeUtil();
+
+        for (int num = 0; num < 1000; num++) {
+            BaseEntity baseEntity = someUtil.randomPerson(num);
+//            System.out.println(baseEntity);
+            dataService.insert(baseEntity);
         }
     }
 
